43889beb2edb3c5670bf915a6e15644ad0bdf55f
Tarvent <chenduo1026@gmail.com>
2022-06-05 19:58:46 +0900

    wangyiyunapi

---
+++ README.md
@@ -0,0 +1,39 @@
+# wangyiyun-app(mobile)
+
+---
+vue3,element.ui
+
+## API
+Backend interface
+网易云API
+https://binaryify.github.io/NeteaseCloudMusicApi/#/?id=安装
+### インストール
+```
+git clone git@github.com:Binaryify/NeteaseCloudMusicApi.git
+npm install
+````
+### 運行
+```
+node app.js
+```
+
+
+
+
+## Project 運行
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/components/home/TopNav.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="topNav">
+    <div class="topLeft">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-31liebiao"></use>
+      </svg>
+    </div>
+    <div class="topContent">
+      <span>MY</span>
+      <span class="active">DISCOVER</span>
+      <span>COMMUNITY</span>
+      <span>VIDEO</span>
+    </div>
+    <div class="topRight">
+      <svg class="icon" aria-hidden="true">
+        <use xlink:href="#icon-sousuo"></use>
+      </svg>
+    </div>
+  </div>
+</template>
+<style lang="less" scoped>
+.topNav {
+  width: 100%;
+  height: 1rem;
+  padding: 0.2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .topContent {
+    width: 80%;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 0.27rem;
+    .active {
+      font-weight: 900;
+    }
+  }
+}
+</style>
